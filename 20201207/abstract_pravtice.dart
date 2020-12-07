a6447ca3f4cf05b6baee4f91818f19477b9c57b9
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2020-12-07 22:08:49 +0900

    Create abstract_pravtice.dart

---
+++ 20201207/abstract_pravtice.dart
@@ -0,0 +1,41 @@
+void main(){
+  for(var i = 0; i < 4; i++){
+    print('hello $i');
+  }
+  var list = [1,2,3,4];
+  list
+    ..add(5)
+    ..addAll([6,7,8]);
+    print(list);
+    String someString = "This is a String";
+  
+  print("The string value is: $someString "); 
+   // prints The string value is: This is a String
+
+  print("The length of the string is: ${someString.length} "); 
+   // prints The length of the string is: 16
+   new MainPart().describeWithEmphasis();
+   new SubPart().describeWithEmphasis();
+}
+
+abstract class Describable {
+  void describe();
+  void describeWithEmphasis(){
+    print('1');
+    describe();
+    print('2');
+  }
+}
+
+class MainPart extends Describable{
+  void describe() => {print('MainPart')};
+}
+
+class SubPart implements Describable{
+  void describe() => print('SubPart');
+  void describeWithEmphasis(){
+    print('3');
+    describe();
+    print('4');
+  }
+}
